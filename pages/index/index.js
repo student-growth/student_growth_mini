6f794de83398d05fdfc951ebd499174fb3a77d82
lee <lee@gmail.com>
2020-05-31 02:24:45 +0800

    add background image

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,39 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+//Page Object
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    
   },
-  //事件处理函数
- 
-})
+  //options(Object)
+  onLoad: function(options) {
+    
+  },
+  onReady: function() {
+    
+  },
+  onShow: function() {
+    
+  },
+  onHide: function() {
+
+  },
+  onUnload: function() {
+
+  },
+  onPullDownRefresh: function() {
+
+  },
+  onReachBottom: function() {
+
+  },
+  onShareAppMessage: function() {
+
+  },
+  onPageScroll: function() {
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item) {
+
+  }
+});
+  
